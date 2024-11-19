6cc62e2533f371ebedb347ffb252973dabefb184
Emerson Sormany <sormanyemerson@gmail.com>
2024-11-19 16:41:58 +0000

    primeira resolução

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 
+const http = require('http');
+const port = 3000 
+const ip = 'localhost'
+
+const server = http.createServer(
+    (_, res) => {
+        res.end({
+            "nome": "Emerson Sormany",
+            "idade": "30",
+            "profissao": "Desenvolvedor"
+        })}
+)
+
+server.listen(port || 3000, ip, ()=> { console.log(`Servidor rodando em http://${ip}:${port}`)})
